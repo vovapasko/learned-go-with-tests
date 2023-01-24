07acb5f4b87ee4ba0e4d4c2f52b840bbb8586085
vovapasko <vovapasko1699@gmail.com>
2023-01-24 13:09:17 +0100

    completed SumAll function and test for it

--- arrays_slices/arrays.go
+++ arrays_slices/arrays.go
@@ -1,9 +1,17 @@
 package arrays_slices
 
-func Sum(numbers [5]int) (sum int) {
+func Sum(numbers []int) (sum int) {
 	sum = 0
 	for _, number := range numbers {
 		sum += number
 	}
 	return
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	var summedArray []int
+	for _, collection := range numbersToSum {
+		summedArray = append(summedArray, Sum(collection))
+	}
+	return summedArray
 }
--- arrays_slices/arrays_test.go
+++ arrays_slices/arrays_test.go
@@ -1,12 +1,25 @@
 package arrays_slices
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
-	numbers := [5]int{1, 2, 3, 4, 5}
-	actual := Sum(numbers)
-	expected := 15
-	if actual != expected {
-		t.Errorf("Actual %d, but expected %d, given %v", actual, expected, numbers)
+	t.Run("run with array size 5", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
+		actual := Sum(numbers)
+		expected := 15
+		if actual != expected {
+			t.Errorf("Actual %d, but expected %d, given %v", actual, expected, numbers)
+		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	actual := SumAll([]int{1, 2}, []int{3, 9})
+	expected := []int{3, 12}
+	if !reflect.DeepEqual(actual, expected) {
+		t.Errorf("Expected %v, but got %v", expected, actual)
 	}
 }
