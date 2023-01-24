cb6705ee98f92e63b2b70b8dbde8842a0bc29efb
vovapasko <vovapasko1699@gmail.com>
2023-01-24 16:04:10 +0100

    Provided pointers to Deposit and Balance methods, provided Bitcoin type

---
+++ pointers_errors/code_test.go
@@ -0,0 +1,16 @@
+package pointers_errors
+
+import "testing"
+
+func TestWallet(t *testing.T) {
+	wallet := Wallet{}
+
+	wallet.Deposit(Bitcoin(10))
+	got := wallet.Balance()
+	want := Bitcoin(10)
+
+	if got != want {
+		t.Errorf("got %d want %d", got, want)
+	}
+
+}
