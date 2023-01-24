537c312b8cf2bd2a181ed3cdbbde0a56f5a391b4
vovapasko <vovapasko1699@gmail.com>
2023-01-24 16:17:35 +0100

    made refactoring

--- pointers_errors/code_test.go
+++ pointers_errors/code_test.go
@@ -3,26 +3,26 @@ package pointers_errors
 import "testing"
 
 func TestWallet(t *testing.T) {
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
+		if got != want {
+			t.Errorf("got %s want %s", got, want)
+		}
+	}
 	t.Run("deposit", func(t *testing.T) {
 		wallet := Wallet{}
 
 		wallet.Deposit(Bitcoin(10))
-		got := wallet.Balance()
 		want := Bitcoin(10)
-
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, want)
 	})
 
 	t.Run("withdraw", func(t *testing.T) {
 		wallet := Wallet{balance: 100}
 		wallet.Withdraw(10)
-		got := wallet.Balance()
 		want := Bitcoin(90)
 
-		if got != want {
-			t.Errorf("got %s want %s", got, want)
-		}
+		assertBalance(t, wallet, want)
 	})
 }
