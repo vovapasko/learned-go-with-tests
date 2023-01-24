63a997e283b491610103204402055ea7a364b4bd
vovapasko <vovapasko1699@gmail.com>
2023-01-24 14:26:03 +0100

    provided perimeter and test for it

---
+++ structs_methods_interfaces/geometry.go
@@ -0,0 +1,5 @@
+package structs_methods_interfaces
+
+func Perimeter(width, height float64) float64 {
+	return (width + height) * 2
+}
---
+++ structs_methods_interfaces/geometry_test.go
@@ -0,0 +1,13 @@
+package structs_methods_interfaces
+
+import "testing"
+
+func TestPerimeter(t *testing.T) {
+	got := Perimeter(10.0, 10.0)
+	want := 40.0
+
+	if got != want {
+		t.Errorf("Got %.2f, want %.2f", got, want)
+
+	}
+}
