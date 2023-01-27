9ff59e6e86f8deb3a520c3abe81e6547cd74be1a
vovapasko <vovapasko1699@gmail.com>
2023-01-27 15:39:15 +0100

    provided channels for map handling

--- concurrency/websites.go
+++ concurrency/websites.go
@@ -1,19 +1,24 @@
 package concurrency
 
-import (
-	"time"
-)
-
 type WebsiteChecker func(string) bool
+
+type result struct {
+	name  string
+	value bool
+}
 
 func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
 	results := make(map[string]bool)
-
+	resultsChanel := make(chan result)
 	for _, url := range urls {
 		go func(url string) {
-			results[url] = wc(url)
+			resultsChanel <- result{url, wc(url)}
 		}(url)
 	}
-	time.Sleep(2 * time.Millisecond)
+	for i := 0; i < len(urls); i++ {
+		r := <-resultsChanel
+		results[r.name] = r.value
+	}
+
 	return results
 }
