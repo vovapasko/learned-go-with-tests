a8037a24d03dd33485aff78259d194417b7d2724
vovapasko <vovapasko1699@gmail.com>
2023-01-27 15:30:15 +0100

    added goroutines in check website

---
+++ concurrency/websites.go
@@ -0,0 +1,19 @@
+package concurrency
+
+import (
+	"time"
+)
+
+type WebsiteChecker func(string) bool
+
+func CheckWebsites(wc WebsiteChecker, urls []string) map[string]bool {
+	results := make(map[string]bool)
+
+	for _, url := range urls {
+		go func(url string) {
+			results[url] = wc(url)
+		}(url)
+	}
+	time.Sleep(2 * time.Millisecond)
+	return results
+}
