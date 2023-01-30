5fd27f58f6197017e05a06a66ce315fee64d9f12
vovapasko <vovapasko1699@gmail.com>
2023-01-30 14:09:44 +0100

    refactored test, provided checking for inner struct type

--- reflection/walk_test.go
+++ reflection/walk_test.go
@@ -5,6 +5,16 @@ import (
 	"testing"
 )
 
+type Person struct {
+	Name    string
+	Profile Profile
+}
+
+type Profile struct {
+	Age  int
+	City string
+}
+
 func TestWalk(t *testing.T) {
 
 	testCases := []struct {
@@ -34,6 +44,14 @@ func TestWalk(t *testing.T) {
 				Age  int
 			}{"Chris", 33},
 			[]string{"Chris"},
+		},
+		{
+			"nested fields",
+			Person{
+				"Chris",
+				Profile{33, "London"},
+			},
+			[]string{"Chris", "London"},
 		},
 	}
 
