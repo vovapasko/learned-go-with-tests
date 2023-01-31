cf294d116c2ffb4dcca2859adb827d9a2eb14d06
vovapasko <vovapasko1699@gmail.com>
2023-01-31 16:33:28 +0100

    provided extracting title from files

--- blogposts/blogposts.go
+++ blogposts/blogposts.go
@@ -1,10 +1,16 @@
 package blogposts
 
 import (
+	"errors"
+	"io"
 	"io/fs"
+	"strings"
 )
 
+const IndexOutOfRangeError = "Index out of range"
+
 type BlogPost struct {
+	Title string
 }
 
 func NewPostsFromFs(filesystem fs.FS) ([]BlogPost, error) {
@@ -14,8 +20,39 @@ func NewPostsFromFs(filesystem fs.FS) ([]BlogPost, error) {
 	}
 
 	var posts []BlogPost
-	for range dir {
-		posts = append(posts, BlogPost{})
+	for _, filename := range dir {
+		post, err := getPost(filesystem, filename)
+		if err != nil {
+			return nil, err
+		}
+		posts = append(posts, post)
 	}
 	return posts, nil
+}
+
+func getPost(filesystem fs.FS, filename fs.DirEntry) (BlogPost, error) {
+	postFile, err := filesystem.Open(filename.Name())
+	if err != nil {
+		return BlogPost{}, err
+	}
+	defer postFile.Close()
+
+	postData, err := io.ReadAll(postFile)
+	if err != nil {
+		return BlogPost{}, err
+	}
+	title, err := extractTitle(string(postData))
+	if err != nil {
+		return BlogPost{}, err
+	}
+	post := BlogPost{Title: title}
+	return post, nil
+}
+
+func extractTitle(data string) (string, error) {
+	splitString := strings.Split(data, "Title: ")
+	if len(splitString) < 2 {
+		return "", errors.New(IndexOutOfRangeError)
+	}
+	return splitString[1], nil
 }
--- blogposts/blogposts_test.go
+++ blogposts/blogposts_test.go
@@ -2,23 +2,52 @@ package blogposts_test
 
 import (
 	"github.com/vovapasko/blogposts"
+	"reflect"
 	"testing"
 	"testing/fstest"
 )
 
 func TestNewBlogPosts(t *testing.T) {
-	fs := fstest.MapFS{
-		"hello world.md":  {Data: []byte("hi")},
-		"hello-world2.md": {Data: []byte("hola")},
-	}
-	posts, err := blogposts.NewPostsFromFs(fs)
+	t.Run("test extracting tests from file system", func(t *testing.T) {
+		fs := fstest.MapFS{
+			"hello world.md":  {Data: []byte("hi")},
+			"hello-world2.md": {Data: []byte("hola")},
+		}
+		_, err := blogposts.NewPostsFromFs(fs)
 
+		assertError(t, err)
+	})
+	t.Run("test specific post after extracting", func(t *testing.T) {
+		fs := fstest.MapFS{
+			"hello world.md":  {Data: []byte("Title: Post 1")},
+			"hello-world2.md": {Data: []byte("Title: Post 2")},
+		}
+		posts, err := blogposts.NewPostsFromFs(fs)
+		got := posts[0]
+		want := blogposts.BlogPost{Title: "Post 1"}
+
+		assertNoError(t, err)
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %+v, want %+v", got, want)
+		}
+
+		if len(posts) != len(fs) {
+			t.Errorf("got %d posts, expected %d posts", len(posts), len(fs))
+		}
+
+	})
+
+}
+
+func assertError(t *testing.T, err error) {
+	if err == nil {
+		t.Error("Should give an error")
+	}
+}
+
+func assertNoError(t *testing.T, err error) {
 	if err != nil {
 		t.Fatal(err)
 	}
-
-	if len(posts) != len(fs) {
-		t.Errorf("got %d posts, expected %d posts", len(posts), len(fs))
-	}
-
 }
