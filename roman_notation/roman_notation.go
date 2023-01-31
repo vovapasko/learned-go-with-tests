5285e6b0ec14881412811639d3d1e0d25310460a
vovapasko <vovapasko1699@gmail.com>
2023-01-31 13:10:20 +0100

    added test case, provided tests as map based

--- roman_notation/roman_notation.go
+++ roman_notation/roman_notation.go
@@ -1,5 +1,8 @@
 package roman_notation
 
 func ConvertToRoman(arabic int) string {
+	if arabic == 2 {
+		return "II"
+	}
 	return "I"
 }
--- roman_notation/roman_notation_test.go
+++ roman_notation/roman_notation_test.go
@@ -3,10 +3,23 @@ package roman_notation
 import "testing"
 
 func TestRomanNumerals(t *testing.T) {
-	got := ConvertToRoman(1)
-	want := "I"
+	testCases := []struct {
+		Description string
+		Arabic      int
+		Want        string
+	}{
+		{"1 gets converted to I", 1, "I"},
+		{"2 gets converted to II", 2, "II"},
+	}
 
-	if got != want {
-		t.Errorf("got %q, want %q", got, want)
+	for _, testCase := range testCases {
+		t.Run(testCase.Description, func(t *testing.T) {
+			got := ConvertToRoman(testCase.Arabic)
+			want := testCase.Want
+
+			if got != want {
+				t.Errorf("got %q, want %q", got, want)
+			}
+		})
 	}
 }
