a6e2d52168bc5ed0e92359f39287dd3eb3eaf8d6
vovapasko <vovapasko1699@gmail.com>
2023-01-31 15:24:37 +0100

    remastered converting seconds to radians

--- clockface/clockface.go
+++ clockface/clockface.go
@@ -1,6 +1,9 @@
 package clockface
 
-import "time"
+import (
+	"math"
+	"time"
+)
 
 type Point struct {
 	X float64
@@ -9,4 +12,8 @@ type Point struct {
 
 func SecondHand(time time.Time) Point {
 	return Point{150, 150 - 90}
+}
+
+func secondsInRadians(t time.Time) float64 {
+	return math.Pi / (30 / float64(t.Second()))
 }
