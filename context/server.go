93410e6f87b762b21247a3b29b7a22eace92d963
vovapasko <vovapasko1699@gmail.com>
2023-01-31 11:25:53 +0100

    provided handling cancelling request from the context

--- context/server.go
+++ context/server.go
@@ -7,10 +7,12 @@ import (
 
 type Store interface {
 	Fetch() string
+	Cancel()
 }
 
 func Server(store Store) http.HandlerFunc {
 	return func(writer http.ResponseWriter, request *http.Request) {
+		store.Cancel()
 		_, _ = fmt.Fprintf(writer, store.Fetch())
 	}
 }
--- context/server_test.go
+++ context/server_test.go
@@ -1,29 +1,58 @@
 package context
 
 import (
+	"context"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 )
 
-type StubStore struct {
-	response string
+type SpyStore struct {
+	response  string
+	cancelled bool
 }
 
-func (s *StubStore) Fetch() string {
+func (s *SpyStore) Fetch() string {
+	time.Sleep(100 * time.Millisecond)
 	return s.response
 }
 
+func (s *SpyStore) Cancel() {
+	s.cancelled = true
+}
+
 func TestServer(t *testing.T) {
-	data := "hello world"
-	server := Server(&StubStore{data})
+	t.Run("test when everything goes well", func(t *testing.T) {
+		data := "hello world"
+		server := Server(&SpyStore{response: data})
 
-	request := httptest.NewRequest(http.MethodGet, "/", nil)
-	response := httptest.NewRecorder()
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+		response := httptest.NewRecorder()
 
-	server.ServeHTTP(response, request)
-	got := response.Body.String()
-	if got != data {
-		t.Errorf("got %s, want %s", got, data)
-	}
+		server.ServeHTTP(response, request)
+		got := response.Body.String()
+		if got != data {
+			t.Errorf("got %s, want %s", got, data)
+		}
+	})
+	t.Run("tells store to cancel work if request is cancelled", func(t *testing.T) {
+		data := "hello world"
+		store := &SpyStore{response: data}
+
+		server := Server(store)
+
+		request := httptest.NewRequest(http.MethodGet, "/", nil)
+		cancellingCtx, cancel := context.WithCancel(request.Context())
+		time.AfterFunc(5*time.Millisecond, cancel)
+		request = request.WithContext(cancellingCtx)
+
+		response := httptest.NewRecorder()
+
+		server.ServeHTTP(response, request)
+		if !store.cancelled {
+			t.Error("Store was not told to cancel")
+		}
+
+	})
 }
