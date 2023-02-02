089972721c7dbd49e9d63f401a0a030f47a5fd4f
vovapasko <vovapasko1699@gmail.com>
2023-02-02 13:30:59 +0100

    refactored blogrenderer.go, introduced html/template rendering

--- blogrenderer/blogrenderer.go
+++ blogrenderer/blogrenderer.go
@@ -1,33 +1,22 @@
 package blogrenderer
 
 import (
-	"fmt"
+	"html/template"
 	"io"
 	"my-super-project/blogposts"
 )
 
+const (
+	postTemplate = `<h1>{{.Title}}</h1><p>{{.Description}}</p>Tags: <ul>{{range .Tags}}<li>{{.}}</li>{{end}}</ul>`
+)
+
 func Render(w io.Writer, p blogposts.BlogPost) error {
-	_, err := fmt.Fprintf(w, "<h1>%s</h1><p>%s</p>", p.Title, p.Description)
+	temp, err := template.New("blog").Parse(postTemplate)
 	if err != nil {
 		return err
 	}
-
-	_, err = fmt.Fprint(w, "Tags: <ul>")
-	if err != nil {
+	if err := temp.Execute(w, p); err != nil {
 		return err
 	}
-
-	for _, tag := range p.Tags {
-		_, err = fmt.Fprintf(w, "<li>%s</li>", tag)
-		if err != nil {
-			return err
-		}
-	}
-
-	_, err = fmt.Fprint(w, "</ul>")
-	if err != nil {
-		return err
-	}
-
 	return nil
 }
