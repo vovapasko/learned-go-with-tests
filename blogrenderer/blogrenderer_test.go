2c6ec903f96c9827cd66a7548a26d5708055b23b
vovapasko <vovapasko1699@gmail.com>
2023-02-02 13:52:41 +0100

    provided approval test

--- blogrenderer/blogrenderer_test.go
+++ blogrenderer/blogrenderer_test.go
@@ -2,6 +2,7 @@ package blogrenderer
 
 import (
 	"bytes"
+	approvals "github.com/approvals/go-approval-tests"
 	"my-super-project/blogposts"
 	"testing"
 )
@@ -18,17 +19,12 @@ func TestRenderer(t *testing.T) {
 
 	t.Run("it converts a single post into HTML", func(t *testing.T) {
 		buf := bytes.Buffer{}
-		err := Render(&buf, aPost)
 
-		if err != nil {
+		if err := Render(&buf, aPost); err != nil {
 			t.Fatal(err)
 		}
 
-		got := buf.String()
-		want := `<h1>hello world</h1><p>This is a description</p>Tags: <ul><li>go</li><li>tdd</li></ul>`
+		approvals.VerifyString(t, buf.String())
 
-		if got != want {
-			t.Errorf("got '%s' want '%s'", got, want)
-		}
 	})
 }
--- go.mod
+++ go.mod
@@ -2,11 +2,4 @@ module my-super-project
 
 go 1.18
 
-require rsc.io/quote v1.5.2
-
-require (
-	github.com/yuin/goldmark v1.4.13 // indirect
-	golang.org/x/sys v0.4.0 // indirect
-	golang.org/x/text v0.0.0-20170915032832-14c0d48ead0c // indirect
-	rsc.io/sampler v1.3.0 // indirect
-)
+require github.com/approvals/go-approval-tests v0.0.0-20220530063708-32d5677069bd
