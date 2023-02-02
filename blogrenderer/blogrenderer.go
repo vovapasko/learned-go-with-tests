8bc324d07847cdfa4fc5c27a19d350cf1ec3d54c
vovapasko <vovapasko1699@gmail.com>
2023-02-02 13:38:51 +0100

    provided embed and its template instead row html in code

--- blogrenderer/blogrenderer.go
+++ blogrenderer/blogrenderer.go
@@ -1,21 +1,23 @@
 package blogrenderer
 
 import (
+	"embed"
 	"html/template"
 	"io"
 	"my-super-project/blogposts"
 )
 
-const (
-	postTemplate = `<h1>{{.Title}}</h1><p>{{.Description}}</p>Tags: <ul>{{range .Tags}}<li>{{.}}</li>{{end}}</ul>`
+var (
+	//go:embed templates/*
+	postTemplates embed.FS
 )
 
 func Render(w io.Writer, p blogposts.BlogPost) error {
-	temp, err := template.New("blog").Parse(postTemplate)
+	templ, err := template.ParseFS(postTemplates, "templates/*.gohtml")
 	if err != nil {
 		return err
 	}
-	if err := temp.Execute(w, p); err != nil {
+	if err := templ.Execute(w, p); err != nil {
 		return err
 	}
 	return nil
